485d7f5864140e3ff7b8f6cf9dceeef42a873485
gorzbi <62179493+gorzbi@users.noreply.github.com>
2022-02-02 13:49:41 +0100

    Update Leba.java

--- src/test/java/POPsimple/tests/Leba.java
+++ src/test/java/POPsimple/tests/Leba.java
@@ -8,7 +8,7 @@ public class Leba extends Configuration{
     @Test
     public void testPierwszy() {
 
-        Google wyszukiwanie = new Google(firefox); // stworzenie obiektu na podstawie klasy, której ma dotyczy test z parametrem z konfiguracji
+        Google wyszukiwanie = new Google(firefox); // stworzenie obiektu na podstawie klasy, której ma dotyczyć test (odwołanie do klasy z pages o nazwie Google) z parametrem z konfiguracji
 
         wyszukiwanie.wyszukanieSlowa("Leba"); // odwołanie się do metody wyszukującej slowo i podanie parametru
 
