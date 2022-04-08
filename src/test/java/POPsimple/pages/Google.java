9bbe27a8ce3b70460df0a70b8aacb7b58ed4bb7e
gorzbi <62179493+gorzbi@users.noreply.github.com>
2022-04-08 15:10:00 +0200

    Update Google.java

--- src/test/java/POPsimple/pages/Google.java
+++ src/test/java/POPsimple/pages/Google.java
@@ -29,7 +29,7 @@ public class Google {
 
     private WebDriver firefox;
 
-    // zainicjowanie parametrów
+    // zainicjowanie zidentyfikowanych elementów na stronie
     public Google(WebDriver firefox) { //nazwa taka sama jak klasy w której jest
         PageFactory.initElements(firefox, this);
         this.firefox = firefox;
