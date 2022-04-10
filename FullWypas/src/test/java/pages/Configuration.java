6a8c128c0c9535f75e554be32fa54ce0b419a6b9
gorzbi <zbigniewgornowicz@wp.pl>
2022-04-10 13:08:14 +0200

    1st commit

---
+++ FullWypas/src/test/java/pages/Configuration.java
@@ -0,0 +1,26 @@
+package pages;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Configuration {
+
+    public class BrowserManager {
+
+        @Test
+        public static void Manager() {
+            WebDriverManager.firefoxdriver().setup();
+            WebDriver firefox = new FirefoxDriver();
+            firefox.manage().window().maximize();
+            firefox.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            firefox.get("https://google.com");
+
+            //kom
+        }
+    }
+}
