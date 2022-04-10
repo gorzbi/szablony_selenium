5a1e69c383997d4fd5403a530d31e88e81d0fd58
gorzbi <62179493+gorzbi@users.noreply.github.com>
2022-04-10 12:04:37 +0200

    Add files via upload

---
+++ Proste/test/Dependencje.java
@@ -0,0 +1,82 @@
+import org.testng.annotations.Test;
+
+public class Dependencje {
+
+    @Test
+
+    public void dependencje() {
+
+        /*
+
+        <dependencies >
+
+- dla javy
+        <dependency >
+            <groupId > org.seleniumhq.selenium </groupId >
+            <artifactId > selenium - java </artifactId >
+            <version > 4.1 .0 </version >
+        </dependency >
+
+- dla raportowania testu
+        <dependency >
+            <groupId > org.testng </groupId >
+            <artifactId > testng </artifactId >
+            <version > RELEASE </version >
+            <scope > test </scope >
+        </dependency >
+
+        <dependency >
+            <groupId > com.aventstack </groupId >
+            <artifactId > extentreports </artifactId >
+            <version > 5.0 .9 </version >
+        </dependency >
+
+- konfiguracja webdriver manager
+        <dependency >
+            <groupId > io.github.bonigarcia </groupId >
+            <artifactId > webdrivermanager </artifactId >
+            <version > 5.0 .3 </version >
+            <scope > test </scope >
+        </dependency >
+
+- log4j
+        <dependency>
+            <groupId>org.apache.logging.log4j</groupId>
+            <artifactId>log4j-api</artifactId>
+            <version>2.14.1</version>
+        </dependency>
+
+        <dependency>
+            <groupId>org.apache.logging.log4j</groupId>
+            <artifactId>log4j-core</artifactId>
+            <version>2.14.1</version>
+        </dependency>
+
+- czytanie danych z plików
+        <dependency>
+            <groupId>org.apache.poi</groupId>
+            <artifactId>poi</artifactId>
+            <version>5.1.0</version>
+        </dependency>
+
+        <dependency>
+            <groupId>org.apache.poi</groupId>
+            <artifactId>poi-ooxml</artifactId>
+            <version>5.1.0</version>
+        </dependency>
+
+- robienie screenów full page 
+        <dependency>
+            <groupId>ru.yandex.qatools.ashot</groupId>
+            <artifactId>ashot</artifactId>
+            <version>1.5.4</version>
+        </dependency>
+
+
+    </dependencies >
+
+             */
+
+    }
+
+}
