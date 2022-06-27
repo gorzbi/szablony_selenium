1a94c4104e1538ab45bdc40fa143203eef7e67dd
gorzbi <62179493+gorzbi@users.noreply.github.com>
2022-06-27 15:22:21 +0200

    Add files via upload

---
+++ Proste/test/POPsimple/tests/Configuration.java
@@ -0,0 +1,37 @@
+package POPsimple.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+import java.time.Duration;
+
+public class Configuration {
+
+    protected WebDriver firefox; // stworzenie pola wykorzystywanego w innych metodach w innych klasach
+
+    @BeforeMethod
+    public void start() {
+        WebDriverManager.firefoxdriver().setup();
+        firefox = new FirefoxDriver();
+        firefox.manage().window().maximize();
+        firefox.get("https://www.google.com/");
+
+        System.out.println("Start testu");
+
+        firefox.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //firefox.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @AfterMethod
+    public void end() {
+        System.out.println("Koniec testu");
+        firefox.quit();
+
+    }
+
+}
---
+++ Proste/test/ProsteSzablony/ReadExcel.java
@@ -0,0 +1,44 @@
+package ProsteSzablony;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ReadExcel {
+
+    @Test
+
+    public static void readExcel() throws IOException {
+
+        // Import Excel file
+        File sciezkaPliku = new File("src/test/resources/frazy.xlsx");
+
+        // Load the file
+        FileInputStream loadPlik = new FileInputStream(sciezkaPliku);
+
+        // Load the workbook
+        Workbook workbook = new XSSFWorkbook(loadPlik);
+
+        // pobieramy pierwszy arkusz z pliku
+        Sheet arkusz = workbook.getSheetAt(0);
+
+        int rowCount = arkusz.getLastRowNum(); // sprawdzamy ile jest wierszy w pliku
+
+        for (int i = 1; i <= rowCount; i++) {
+            Row wiersz = arkusz.getRow(i); // pobieramy poszczególne wiersze
+
+            String kol1 = wiersz.getCell(0).getStringCellValue(); // pobieramy wartości z pierwszej kolumny (w pliku jest tylko 1 kolumna)
+            // String kol2 = wiersz.getCell(1).getStringCellValue(); // jakby była druga kolumna
+
+            System.out.println(kol1);
+
+        }
+    }
+}
