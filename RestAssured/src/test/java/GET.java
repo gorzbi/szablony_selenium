2d9985032c01c9156054047afb971f7ce87d43b6
zbigniew.gornowicz <zbigniew.gornowicz@soflab.pl>
2022-06-27 14:50:16 +0200

    GET

--- RestAssured/src/test/java/GET.java
+++ RestAssured/src/test/java/GET.java
@@ -1,16 +1,24 @@
+import org.testng.annotations.Test;
+
 import static io.restassured.RestAssured.when;
 
 public class GET {
 
-    public static void main(String[] args) {
+    @Test
+    public static void get() {
         System.out.println("Nic nie wyświetli");
         when().get("https://jsonplaceholder.typicode.com/posts/1"); // tu wykona tylko request ale nic nam nie wyświetli
 
         System.out.println("Teraz wyświetli body response:");
-        when().get("https://jsonplaceholder.typicode.com/posts/1").then().log().body();
-
+        when().get("https://jsonplaceholder.typicode.com/posts/1").then().log().body(); // nie sprawdza jaki jest status, albo co jest w body
 
         System.out.println("Teraz wyświetli dodatkowo headers:");
         when().get("https://jsonplaceholder.typicode.com/posts/1").then().log().all();
+
+        System.out.println("Z walidacją i jaki status się spodziewamy:");
+        when().get("https://jsonplaceholder.typicode.com/posts/1").then().log().ifValidationFails().statusCode(404); //będzie błąd bo oczekujemy 404 a będzie 200
+
+        System.out.println("POST 101 utworzony przez POST");
+        when().get("https://jsonplaceholder.typicode.com/posts/101").then().log().all();
     }
 }
---
+++ RestAssured/src/test/java/POST.java
@@ -0,0 +1,25 @@
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class POST {
+
+    @Test
+    public static void post() {
+
+        String cialoRequesta = """
+                {
+                    "userId": 111,
+                    "title": "elo2",
+                    "body": "elo2"
+                }""";
+
+        given()
+                .log().all() //pozwoli sprawdzić co dokładnie wysłaliśmy z jakimi parametrami
+                .contentType(ContentType.JSON) // należy ustawić właściwy content type
+                .body(cialoRequesta)
+        .when().post("https://jsonplaceholder.typicode.com/posts")
+        .then().log().all();
+    }
+}
