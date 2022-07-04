7bae378c040e87e624e3048f308e8b579581feab
zbigniew.gornowicz <zbigniew.gornowicz@soflab.pl>
2022-07-04 13:22:52 +0200

    JsonPath update 1.0

--- RestAssured/src/test/java/JsonPath.java
+++ RestAssured/src/test/java/JsonPath.java
@@ -1,7 +1,6 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
-
 import java.util.List;
 import java.util.Map;
 
@@ -10,18 +9,23 @@ public class JsonPath {
     @Test
     public void response(){
 
-        Response odpowiedz = RestAssured.get("http://localhost:3000/posts/1");
+    Response odpowiedz = RestAssured.get("http://localhost:3000/posts/1");
 
-        System.out.println(odpowiedz.asString());
-
+      System.out.println(odpowiedz.asString());
+/*
         String secret = odpowiedz.path("secret"); // pobieramy pole secret
         List<Integer> winningNb = odpowiedz.path("winning-numbers"); // pobieramy listę, która jest cyframi
         String firstWinnerName = odpowiedz.path("winners.name[0]"); // 0 - pierwsze imię na liście winnersów
         String firstWinnerName1 = odpowiedz.path("winners[0].name"); // alternatywny zapis
         String lastWinnerName = odpowiedz.path("winners.name[-1]"); // -1 to ostatnie imię na liście winnersów
         List<String> winnersNames = odpowiedz.path("winners.name"); // lista imion z winnersów
-        Map<String,?> winnerAllFields = odpowiedz.path("winners[0]"); // wszystkie pola pierwszego winnera
-        List<Map<String,?>> allWinners = odpowiedz.path("winners"); // wszyscy ze wszystkimi polami
+        Map<String,?> winnerAllFields = odpowiedz.path("winners[0]"); // wszystkie pola pierwszego winnera; String bo nazwa pola jest stringiem, ? bo wartości mogą być strinkiem, int i inne
+        List<Map<String,?>> allWinners = odpowiedz.path("winners"); // wszyscy ze wszystkimi polami*/
 
+        Map<String,?> winnerInfo = odpowiedz.path("winners.find {it.name=='John'}"); // w winnersach find szuka, it wszędzie pola name z wartością John
+        Integer winnerId = odpowiedz.path("winners.find {it.name=='John'}.winnerId"); // wyciągnie winnerId z winnersów gdzie imie to John
+        Integer maxWinningNumber = odpowiedz.path("winning-numbers.max()"); // max/min liczba ze zbioru
+        Map<String,?> maxWinnerId = odpowiedz.path("winners.max {it.winnerId}"); // wyciągnie całe info o osbie o max id ze zbioru winners
+        Integer moneySum = odpowiedz.path("winners.collect {it.money}.sum()"); // suma money ze zbioru winners
     }
 }
